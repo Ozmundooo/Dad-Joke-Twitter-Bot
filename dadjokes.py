2f63a9e6443f37f0ac46bb8dcfa7f13b02d4a68a
Ozmundooo <osman.ali.dev@gmail.com>
2020-05-22 16:47:44 -0400

    Create dadjokes.py

---
+++ dadjokes.py
@@ -0,0 +1,22 @@
+import tweepy
+import time
+from joke.jokes import icanhazdad
+
+api_key = "API_KEY"
+api_secret_key = "API_SECRET_KEY"
+access_token = "ACCESS_TOKEN"
+access_token_secret = "ACCESS_TOKEN_SECRET"
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(api_key, api_secret_key)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create API object
+api = tweepy.API(auth)
+
+# Tweet Dadjoke
+while True:
+    joke = icanhazdad()
+    api.update_status(joke)
+    print(joke)
+    time.sleep(900)     # Timer 
